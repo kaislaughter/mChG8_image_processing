4a6fa84900a7b3882eb096bfcffa6afb60fb5183
ercdndrs <eric.donders@outlook.com>
2021-08-09 20:01:24 -0400

    Add text annotation for scale bar length
    - Requires the Machine Vision Toolbox

--- cleanComposite.m
+++ cleanComposite.m
@@ -1,5 +1,9 @@
 %% Script for preparing composites for publication
 
+%   Requirements:
+%    - Image Processing Toolbox
+%    - Machine Vision Toolbox
+
 %   Input: exported composite images from EE_disruption_uptake_CZI.m
 %   (or similar).
 %   IMPORTANT: the image format must conform to the following standard:
@@ -7,18 +11,20 @@
 %    - Gal8: green
 %    - Colloids/NPs: magenta (equal red and blue)
 
-%   Output: individual channels plus annotated colocalization maps, in both
-%   the original size and zoomed-in sections, with scale bars.
+%   Output: individual channels plus composite images, in both the original
+%   size and a zoomed-in section, with scale bars.
 
 clc, clear, close all;
 
 %% Config
 %   Fill in this section before each run.
 
-pixelSize = 0.1;  % uM PLACEHOLDER
-zoomedWidth = 100;  % uM
-zoomedHeight = 100;  % uM
-scaleBar = 25;  % uM
+pixelSize = 0.1;  % um PLACEHOLDER
+zoomedWidth = 100;  % um
+zoomedHeight = 100;  % um
+scaleBar = 25;  % um
+p = 25;  % pixels (padding and thickness of scale bar
+textSize = 48;  % point (font size for the scale bar label)
 
 %% Setup
 disp('Choose input directory')
@@ -29,15 +35,14 @@ exportdir = [uigetdir(), filesep]; % Prompts user for output directory
 listing = dir(strcat(workingdir, '*.png'));
 numImages = length(listing);
 
-%%  Load the images
 for i = 1:numImages
     clc;
-    disp(['Processing PNG image ', num2str(i), ' of ', num2str(numImages)]);
+    disp(['Processing image ', num2str(i), ' of ', num2str(numImages)]);
     title = listing(i,1).name(1:end-4);
-    
+    %%  Load the image
     image = imread(strcat(workingdir, title, '.png'));
 
-%%  Get the user to select ROIs for the zoomed sections
+    %%  Get the user to select ROIs for the zoomed sections
     % Display the image and a rectangular ROI that the user can move.
     h = imshow(image);
     sectionROI = images.roi.Rectangle(gca, 'Position',...
@@ -50,13 +55,13 @@ for i = 1:numImages
         pause(0.02);
     end
 
-%%  Export images and insets with added scale bars
+    %%  Export images and insets with added scale bars
     disp('Splitting and cropping the image...');
     % Isolate individual channels
     colloids = image(:, :, 1);
     gal8 = image(:, :, 2);
     nuclei = image(:, :, 3) - colloids;
-    %   Crop the image to the inset.
+    % Crop the image to the inset.
     imageZoomed = image(...
         pos(2)+1:pos(2)+pos(4)-1, pos(1)+1:pos(1)+pos(3)-1, :);
     colloidsZoomed = imageZoomed(:, :, 1);
@@ -67,37 +72,50 @@ for i = 1:numImages
     largeWidth = width(nuclei);
     smallHeight = floor(zoomedHeight/pixelSize);
     smallWidth = floor(zoomedWidth/pixelSize);
+    scaleBarWidth = floor(scaleBar/pixelSize);
     
-    %   Create images for the scale bar for both full and zoomed images.
+    % Create images for the scale bar for both full and zoomed images.
     disp('Adding the scale bar...');
     zerosLarge = zeros(largeHeight, largeWidth, 'uint16');
-    SBL = zerosLarge;
+    SBL = zeros(largeHeight, largeWidth, 3, 'uint16');
     zerosSmall = zeros(smallHeight, smallWidth, 'uint16');
-    SBS = zerosSmall;
-    SBL(largeHeight-50:largeHeight-25,...
-        largeWidth-25-round(scaleBar/pixelSize):largeWidth-25) = 65535;
-    SBS(smallHeight-50:smallHeight-25,...
-        smallWidth-25-round(scaleBar/pixelSize):smallWidth-25) = 65535;
+    SBS = zeros(smallHeight, smallWidth, 3, 'uint16');
+    % Create a white rectangle for the scale bar.
+    SBL(largeHeight-2*p:largeHeight-p,...
+        largeWidth-p-scaleBarWidth:largeWidth-p, :) = 65535;
+    SBS(smallHeight-2*p:smallHeight-p,...
+        smallWidth-p-scaleBarWidth:smallWidth-p, :) = 65535;
+    % Add a text label to the scale bars.
+    SBL = insertText(SBL,...
+        [largeWidth-p-floor(scaleBarWidth/2), largeHeight-2*p],...
+        [num2str(scaleBar) ' µm'], 'TextColor', 'white',...
+        'BoxOpacity', 0, 'FontSize', textSize,...
+        'AnchorPoint', 'CenterBottom');
+    SBS = insertText(SBS,...
+        [smallWidth-p-floor(scaleBarWidth/2), smallHeight-2*p],...
+        [num2str(scaleBar) ' µm'], 'TextColor', 'white',...
+        'BoxOpacity', 0, 'FontSize', textSize,...
+        'AnchorPoint', 'CenterBottom');  
     
-    
-    %   Write the images.
+    % Write the images.
     disp('Writing the images to disk...');
+    % Export full-size image.
     exportBase = strcat(exportdir, title, '_');
-    exportBaseZoomed = strcat(exportBase, 'zoomed_');
-    imwrite(image + cat(3, SBL, SBL, SBL),...
-        strcat(exportBase, 'composite.png'));
-    imwrite(cat(3, colloids + SBL, SBL, colloids + SBL),...
+    imwrite(image + SBL, strcat(exportBase, 'composite.png'));
+    imwrite(cat(3, colloids, zerosLarge, colloids) + SBL,...
         strcat(exportBase, 'colloids.png'));
-    imwrite(cat(3, SBL, gal8 + SBL, SBL), strcat(exportBase, 'gal8.png'));
-    imwrite(cat(3, SBL, SBL, nuclei + SBL),...
+    imwrite(cat(3, zerosLarge, gal8, zerosLarge) + SBL,...
+        strcat(exportBase, 'gal8.png'));
+    imwrite(cat(3, zerosLarge, zerosLarge, nuclei) + SBL,...
         strcat(exportBase, 'nuclei.png'));
-    imwrite(imageZoomed + cat(3, SBS, SBS, SBS),...
-        strcat(exportBaseZoomed, 'composite.png'));
-    imwrite(cat(3, colloidsZoomed + SBS, SBS, colloidsZoomed + SBS),...
+    % Export zoomed-in image.
+    exportBaseZoomed = strcat(exportBase, 'zoomed_');
+    imwrite(imageZoomed + SBS, strcat(exportBaseZoomed, 'composite.png'));
+    imwrite(cat(3, colloidsZoomed, zerosSmall, colloidsZoomed) + SBS,...
         strcat(exportBaseZoomed, 'colloids.png'));
-    imwrite(cat(3, SBS, gal8Zoomed + SBS, SBS),...
+    imwrite(cat(3, zerosSmall, gal8Zoomed, zerosSmall) + SBS,...
         strcat(exportBaseZoomed, 'gal8.png'));
-    imwrite(cat(3, SBS, SBS, nucleiZoomed + SBS),...
+    imwrite(cat(3, zerosSmall, zerosSmall, nucleiZoomed) + SBS,...
         strcat(exportBaseZoomed, 'nuclei.png'));
 end
 disp('Processing complete!');
